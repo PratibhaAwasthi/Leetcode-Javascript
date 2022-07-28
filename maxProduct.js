5621aef96419fb249ee5a1a2e0391a11a18f0f65
Pratibha Awasthi <82785478+PratibhaAwasthi@users.noreply.github.com>
2022-07-27 18:28:33 -0700

    Delete maxProduct.js

--- maxProduct.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxProduct = function (nums) {
-  nums.sort((a, b) => a - b);
-  return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
-};
